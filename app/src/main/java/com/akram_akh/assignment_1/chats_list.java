358a6f51fe26758e66e0422f56a4d918589853c6
akramakh <akram.icode@gmail.com>
2019-10-25 17:56:35 +0300

    initial commit

---
+++ app/src/main/java/com/akram_akh/assignment_1/chats_list.java
@@ -0,0 +1,13 @@
+package com.akram_akh.assignment_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class chats_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chats_list);
+    }
+}
